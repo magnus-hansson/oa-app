7b55cd859732b9199ef030060367895ea42d0967
magnus-hansson <hanssmag+git@gmail.com>
2020-06-03 16:07:19 +0200

    static works but ..

--- api/clubs-get/index.js
+++ api/clubs-get/index.js
@@ -2,6 +2,10 @@ const data = require("../shared/db");
 
 module.exports = async function (context, req) {
   context.log("JavaScript HTTP trigger function processed a request.");
-  let items = await data.getClubs();
-  context.res.status(200).json(items);
+  try {
+    let items = await data.getClubs();
+    context.res.status(200).json(items);
+  } catch (error) {
+    context.res.status(500).send(error);
+  }
 };
